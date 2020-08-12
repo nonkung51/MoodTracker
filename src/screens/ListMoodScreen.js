eb035406aedec8faa6200cab3c3c5ad1e905aab3
Nonthakon Jitchiranant <non_thakon@hotmail.com>
2020-08-12 16:55:01 +0700

    :sparkles: ListMoodScreen show item list

--- src/screens/ListMoodScreen.js
+++ src/screens/ListMoodScreen.js
@@ -1,6 +1,22 @@
 import React, {useEffect} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, FlatList} from 'react-native';
 import {useSelector} from 'react-redux';
+import {ListItem} from 'react-native-elements';
+
+const list = [
+  {
+    name: 'Amy Farha',
+    avatar_url:
+      'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+    subtitle: 'Vice President',
+  },
+  {
+    name: 'Chris Jackson',
+    avatar_url:
+      'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+    subtitle: 'Vice Chairman',
+  },
+];
 
 export default () => {
   const mood = useSelector((state) => state.mood);
@@ -9,9 +25,21 @@ export default () => {
     console.log(mood);
   }, [mood]);
 
+  const keyExtractor = (item, index) => index.toString();
+  const renderItem = ({ item }, index) => {
+    console.log('item', item);
+    return (
+      <ListItem leftAvatar={<Text>😊</Text>} title={item.reason} bottomDivider />
+    );
+  };
+
   return (
     <View>
-      <Text>Hey!</Text>
+      <FlatList
+        keyExtractor={keyExtractor}
+        data={mood.moods}
+        renderItem={renderItem}
+      />
     </View>
   );
 };
